5428a587858c65f996d9bf352c7609d898e62cb0
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2026-08-22 03:21:24 -0500

    comment-purge(P-A): crates/bumbledb/src/api/db/delete.rs restore shredded rustdoc

--- crates/bumbledb/src/api/db/delete.rs
+++ crates/bumbledb/src/api/db/delete.rs
@@ -3,7 +3,6 @@ use crate::error::Result;
 
 impl<S> WriteTx<'_, S> {
     /// # Errors
-
     pub fn delete<'f, F: Fact<'f, Schema = S> + 'f>(
         &mut self,
         facts: impl IntoIterator<Item = &'f F>,
