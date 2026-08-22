f75eb3ddf3110b33ec57ed6194e6e8024d677964
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2026-08-22 05:06:02 -0500

    comment-purge(P-B): crates/bumbledb/src/storage/env/create.rs clippy rustdoc restore

--- crates/bumbledb/src/storage/env/create.rs
+++ crates/bumbledb/src/storage/env/create.rs
@@ -7,7 +7,6 @@ use super::Environment;
 
 impl Environment {
     /// # Errors
-
     /// exists (including as an empty directory), `Lmdb` on any LMDB
     #[cfg_attr(not(test), allow(dead_code))]
     pub fn create(path: &Path, schema: &Schema) -> Result<Self> {
