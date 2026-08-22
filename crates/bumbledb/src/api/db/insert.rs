1ed09783e46b4ead2297e76b7a331438ee1f2794
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2026-08-22 05:05:58 -0500

    comment-purge(P-A): crates/bumbledb/src/api/db/insert.rs clippy rustdoc restore

--- crates/bumbledb/src/api/db/insert.rs
+++ crates/bumbledb/src/api/db/insert.rs
@@ -3,7 +3,6 @@ use crate::error::Result;
 
 impl<S> WriteTx<'_, S> {
     /// # Errors
-
     /// after a prefix entered the delta.
     pub fn insert<'f, F: Fact<'f, Schema = S> + 'f>(
         &mut self,
